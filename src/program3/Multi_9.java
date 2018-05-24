392744d3bf7442a2b7a84f3ca560b47474fd541a
1152874847@qq.com <1152874847@qq.com>
2018-05-24 14:53:34 +0800

    九九乘法表

---
+++ src/program3/Multi_9.java
@@ -0,0 +1,30 @@
+package program3;
+
+
+public class Multi_9 {
+
+	public static void main(String[] args) {
+
+	int i=1;
+	int j=1;
+	
+	for(i=1;i<=9;i++) {
+		for(j=1;j<=9;j++) {
+			System.out.print("\t"+i+"x"+j+"="+(i*j));
+		}
+		
+		System.out.println();
+		
+		
+	}
+			
+		 
+			 
+		 
+		 
+		
+		
+
+	}
+
+}
